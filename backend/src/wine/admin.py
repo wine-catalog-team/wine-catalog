dcc2860aef628dc73423ae6d747e34b396bd0915
ComfortCityBud <gleb@gmail.com>
2025-05-21 15:02:22 +0300

    Added models for wine

--- backend/src/wine/admin.py
+++ backend/src/wine/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from wine.models import Wine
+
+
+@admin.register(Wine)
+class WineAdmin(admin.ModelAdmin):
+    list_display = [
+        "type",
+        "grape_variety",
+        "vintage",
+        "alcohol",
+        "sugar",
+        "acidity",
+        "bottle_volume",
+        "serving_temperature",
+        "aging",
+        "pairing",
+        "aroma",
+        "taste",
+        "color",
+    ]
