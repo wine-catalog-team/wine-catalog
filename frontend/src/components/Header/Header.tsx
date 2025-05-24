19ede6159fd63a40dcdd8d47d03d5f2ba33d9166
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-05-24 10:50:18 +0300

    header and hero desktop version

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -6,22 +6,31 @@ const Header = () => {
     <header className={styles.header}>
       <div className="container">
         <div className={styles.wrapper}>
-          <span className={styles.logo}>Logo</span>
+          <Link to="/" className={styles.logo}>
+            Logo
+          </Link>
 
           <span className={styles.burger}>
             <img src="./img/icons/burger.svg" alt="burger" />
           </span>
 
-          {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">HomePage</Link>
-            </li>
-            <li>
-              <Link to="/catalog">CatalogPage</Link>
-            </li>
-          </ul>
-        </nav> */}
+          <nav className={styles.nav}>
+            <ul>
+              <li>
+                <Link to="/">Про нас</Link>
+              </li>
+              <li>
+                <Link to="/catalog">Колекція вин</Link>
+              </li>
+              <li>
+                <Link to="/catalog">Де купити</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <a href="#contact" className={styles.btn}>
+            Зв’язатися з нами
+          </a>
         </div>
       </div>
     </header>
