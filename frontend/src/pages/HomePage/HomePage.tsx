cfe52628770e423333b8dd0bb5d63c73a72f6e85
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-05-24 12:33:12 +0300

    Branch element

--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,5 @@
 import AboutUs from '../../components/AboutUs';
+import Branch from '../../components/Branch';
 import Hero from '../../components/Hero';
 import styles from './HomePage.module.scss';
 
@@ -7,6 +8,7 @@ const HomePage = () => {
     <div className={styles.home}>
       <Hero />
       <AboutUs />
+      <Branch />
     </div>
   );
 };
